15ab9bdaf5cd3d34dd45dad927a379d1f308722b
Kuroneko <18299687659@163.com>
2021-04-23 15:02:34 +0800

    no message

--- src/COMMON/Components/makePromise/index.tsx
+++ src/COMMON/Components/makePromise/index.tsx
@@ -5,11 +5,11 @@
  */
 
 declare namespace MakePromsie {
-	export type main = <T extends {}>( callback: (resolve,reject) => any , ifValueCanBeReplace: boolean ) => {
-		promise: Promise<T> ,
-		resolve: (value:any) => void,
-		reject: (reason:any) => void ,
-		getStatus : () => status ,
+	export type main = <T extends {}>( callback: (resolve,reject) => any , onStatusChange : (status:status) => any ) => {
+		promise: Promise<T|any> ,
+		resolve: (value:any) => any,
+		reject: (reason:any) => any ,
+		getStatus : status ,
 		
 	};
 	export type status =
@@ -18,39 +18,49 @@ declare namespace MakePromsie {
 		| "rejected";
 	
 }
-export const makePromise : MakePromsie.main = <T extends {}>( callback:(resolve,reject) => any = () => null , ifValueCanBeReplace: boolean = false ) => {
+export const makePromise : MakePromsie.main = <T extends {}>( callback:(resolve,reject) => any = () => null , onStatusChange = (status) => null ) => {
 	let resolve , reject ;
 	let status : MakePromsie.status = "pending";
-	const promise = new Promise<T>( ( $resolve , $reject ) => {
-		resolve = $reject;
-		reject = $reject;
+	let currentPromise = new Promise<T | any>( ( $resolve , $reject ) => {
+		
+		resolve = (value) => {
+			if(status === 'resolved') {
+				currentPromise = Promise.resolve( value );
+				return true;
+			}else if(status === 'pending'){
+				$resolve(value);
+				status = 'resolved';
+				onStatusChange?.(status);
+				return undefined;
+			}
+		};
+		
+		reject = (rejectValue) => {
+			if(status === 'rejected') {
+				currentPromise = Promise.reject( rejectValue );
+				return true;
+			}else if(status === 'pending'){
+				$reject(rejectValue);
+				status = 'rejected';
+				onStatusChange?.(status);
+				return undefined;
+			}
+		};
+		
 		callback?.(
 			resolve ,
 			reject,
 		);
 	} );
 	
-	return {
-		promise ,
-		resolve : (value) => {
-			resolve(value);
-			status = "resolved";
-		},
-		reject : (reason) => {
-			reject( reason );
-			status = "rejected";
-		},
-		getStatus : () => status ,
-	};
-};
-
-export const newMakePromise = () => {
-	
-	
-	
-	return new Proxy({},{
-		get : (target , propKey , receiver) => {
-		
-		},
-	})
+	return new Proxy({} as ReturnType<MakePromsie.main>, {
+		get( target , p: PropertyKey , receiver: any ): any {
+			switch ( p ) {
+				case "promise" : return currentPromise;break;
+				case "resolve" : return resolve;break;
+				case "reject" : return reject;break;
+				case "status" : return status;break;
+			}
+		}
+	});
 };
